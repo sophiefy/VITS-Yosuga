5d4104607e479892e1a9bfd44b2c1613e8d962b6
FrancisHu <87693204+Francis-Komizu@users.noreply.github.com>
2022-08-15 17:44:00 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("--out_extension", default="cleaned")
   parser.add_argument("--text_index", default=2, type=int)
-  parser.add_argument("--filelists", nargs="+", default=["filelists/val_filelist.txt", "filelists/trainfilelist.txt"])
+  parser.add_argument("--filelists", nargs="+", default=["filelists/val_filelist.txt", "filelists/train_filelist.txt"])
   parser.add_argument("--text_cleaners", nargs="+", default=["japanese_cleaners"])
 
   args = parser.parse_args()
